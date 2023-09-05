7624f76079ee3a2bc5a71f52015255c712595ca2
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-09-05 23:35:26 +0200

    complete the pagination logic

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import {FakedData} from './FakedData';
 const App = () => {
 
   return (
-    <div style={{width:"90%", margin:"auto"}}>
+    <div style={{width:"90%",height:"80vh", margin:"auto"}}>
       <h1>Sample Data Table</h1>
       <TableGrid data={FakedData.data} columns={FakedData.column} />
     </div>
--- src/TableGrid.js
+++ src/TableGrid.js
@@ -1,31 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './TableGrid.css';
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 import { BiFirstPage, BiLastPage } from 'react-icons/bi';
 
 
 const DataTable = ({ data, columns }) => {
-    const [orderedData, setOrderedData] = useState("")
+    const [orderedData, setOrderedData] = useState("");
+    const [page, setPage] = useState(0);
+
 
     function pagination(number) {
-        let index=0;
+        let index = 0;
         let newData = [];
         for (let i = 0; i < (Math.ceil(data.length / number)); i++) {
-            let eachElement=[];
+            let eachElement = [];
             for (let j = 0; j < number; j++) {
-                
-                if(data[index]){
+
+                if (data[index]) {
                     eachElement.push(data[index]);
-                    index=index+1;
-                }  
+                    index = index + 1;
+                }
             }
             newData.push(eachElement);
-            eachElement=[];
-            
+            eachElement = [];
+
         }
         setOrderedData(newData);
-        console.log(newData);
+        setPage(0);
+
     }
+
+    function handlePageChange(number) {
+        setPage(number);
+    }
+
+    useEffect(() => {
+        pagination(5)
+    }, []);
+
+    console.log("page", page);
+    console.log("orderedData", orderedData);
 
     return (
         <section className='tableGridWhole'>
@@ -39,7 +53,7 @@ const DataTable = ({ data, columns }) => {
                     </tr>
                 </thead>
                 <tbody className='tableGridBody'>
-                    {data.map((row, rowIndex) => (
+                    {orderedData[page] && orderedData[page].map((row, rowIndex) => (
                         <tr key={rowIndex} className='tableGridBodyRow'>
                             {columns.map((column, colIndex) => (
                                 <td key={colIndex} className='tableGridBodyColumn'>{row[column.field]}</td>
@@ -51,22 +65,38 @@ const DataTable = ({ data, columns }) => {
             <div className='tableGridPagination'>
                 <div><p>{data.length} Total</p></div>
                 <div>
-                    <button><BiFirstPage className='tableGridIcon' /></button>
-                    <button><IoIosArrowBack className='tableGridIcon' /></button>
-                    <button className='tableGridPaginationSelected'>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button><IoIosArrowForward className='tableGridIcon' /></button>
-                    <button><BiLastPage className='tableGridIcon' /></button>
+                    <div>
+                        <button onClick={() => handlePageChange(0)}>
+                            <BiFirstPage className='tableGridIcon' />
+                        </button>
+                        <button onClick={() => handlePageChange(page - 1)}>
+                            <IoIosArrowBack className='tableGridIcon' />
+                        </button>
+                        {orderedData && orderedData.map((_, index) => (
+                            <button
+                                key={index}
+                                onClick={() => handlePageChange(index)}
+                                className={page === index ? 'tableGridPaginationSelected' : ''}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
+                        <button onClick={() => handlePageChange(page + 1)}>
+                            <IoIosArrowForward className='tableGridIcon' />
+                        </button>
+                        <button onClick={() => handlePageChange(orderedData.length - 1)}>
+                            <BiLastPage className='tableGridIcon' />
+                        </button>
+                    </div>
+                    <div>
+                        <select name="numbersShow" id="numbersShow" className='tableGridSelect'>
+                            <option value="volvo" onClick={() => pagination(5)}>5 items per page</option>
+                            <option value="volvo" onClick={() => pagination(10)}>10 items per page</option>
+                            <option value="volvo" onClick={() => pagination(15)}>15 items per page</option>
+                            <option value="volvo" onClick={() => pagination(20)}>20 items per page</option>
+                        </select>
+                    </div>
 
-                    <select name="numbersShow" id="numbersShow" className='tableGridSelect'>
-                        <option value="volvo" onClick={() => pagination(5)}>5 items per page</option>
-                        <option value="volvo" onClick={() => pagination(10)}>10 items per page</option>
-                        <option value="volvo" onClick={() => pagination(15)}>15 items per page</option>
-                        <option value="volvo" onClick={() => pagination(20)}>20 items per page</option>
-                    </select>
                 </div>
             </div>
         </section>
